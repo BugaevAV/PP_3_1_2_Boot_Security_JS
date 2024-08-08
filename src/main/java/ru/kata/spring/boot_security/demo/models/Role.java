8b7c9ae3df159b44f88fd3dbea2848484cdbe97b
BugaevAV <iustusvir@gmail.com>
2024-08-08 22:16:32 +0300

    add ROLE_ADMIN and ROLE_USER initializer

--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -18,6 +18,12 @@ public class Role implements GrantedAuthority {
     @ManyToMany(mappedBy = "roles")
     private List<User> users;
 
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
     @Override
     public String getAuthority() {
         return getName();
